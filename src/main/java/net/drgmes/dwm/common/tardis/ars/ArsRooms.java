c97c9413a3a944e953f8ad1bb250af8134da0b4a
Herman Mesky <dr.gmes@gmail.com>
2022-06-07 14:07:52 +0300

    Added more Tech t-junctions

--- src/main/java/net/drgmes/dwm/common/tardis/ars/ArsRooms.java
+++ src/main/java/net/drgmes/dwm/common/tardis/ars/ArsRooms.java
@@ -13,6 +13,8 @@ public class ArsRooms {
     public static final ArsRoom HALLWAY_TECH_LONG = register("hallway_tech_long", "title." + DWM.MODID + ".ars.room.hallway_tech_long", ArsCategories.HALLWAYS_TECH);
     public static final ArsRoom HALLWAY_TECH_JUNCTION_X = register("hallway_tech_junction_x", "title." + DWM.MODID + ".ars.room.hallway_tech_junction_x", ArsCategories.HALLWAYS_TECH);
     public static final ArsRoom HALLWAY_TECH_JUNCTION_T = register("hallway_tech_junction_t", "title." + DWM.MODID + ".ars.room.hallway_tech_junction_t", ArsCategories.HALLWAYS_TECH);
+    public static final ArsRoom HALLWAY_TECH_JUNCTION_T_LEFT = register("hallway_tech_junction_t_left", "title." + DWM.MODID + ".ars.room.hallway_tech_junction_t_left", ArsCategories.HALLWAYS_TECH);
+    public static final ArsRoom HALLWAY_TECH_JUNCTION_T_RIGHT = register("hallway_tech_junction_t_right", "title." + DWM.MODID + ".ars.room.hallway_tech_junction_t_right", ArsCategories.HALLWAYS_TECH);
     public static final ArsRoom HALLWAY_TECH_TURN_LEFT = register("hallway_tech_turn_left", "title." + DWM.MODID + ".ars.room.hallway_tech_turn_left", ArsCategories.HALLWAYS_TECH);
     public static final ArsRoom HALLWAY_TECH_TURN_RIGHT = register("hallway_tech_turn_right", "title." + DWM.MODID + ".ars.room.hallway_tech_turn_right", ArsCategories.HALLWAYS_TECH);
 
