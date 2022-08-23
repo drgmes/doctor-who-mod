1b502e2d8c889425595edad176a4b76ba000bdd4
Herman Mesky <dr.gmes@gmail.com>
2022-08-24 01:30:35 +0300

    Cancel fall damage in a Tardis & Teleport entities to entrance when they out of a Tardis Dim bounds

---
+++ src/main/java/net/drgmes/dwm/mixin/MixinLivingEntity.java
@@ -0,0 +1,34 @@
+package net.drgmes.dwm.mixin;
+
+import net.drgmes.dwm.common.tardis.TardisStateManager;
+import net.drgmes.dwm.utils.helpers.TardisHelper;
+import net.minecraft.entity.LivingEntity;
+import net.minecraft.entity.damage.DamageSource;
+import net.minecraft.server.world.ServerWorld;
+import net.minecraft.util.math.BlockPos;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
+
+@Mixin(LivingEntity.class)
+public class MixinLivingEntity {
+    @Inject(method = "damage", at = @At("HEAD"), cancellable = true)
+    private void damage(DamageSource source, float amount, CallbackInfoReturnable<Boolean> cir) {
+        LivingEntity $this = (LivingEntity) (Object) this;
+        if ($this.world.isClient) return;
+
+        if (TardisHelper.isTardisDimension($this.world)) {
+            if (source.isOutOfWorld()) {
+                TardisStateManager.get((ServerWorld) $this.world).ifPresent((tardis) -> {
+                    BlockPos entrancePosition = tardis.getEntrancePosition();
+                    $this.teleport(entrancePosition.getX() + 0.5, entrancePosition.getY(), entrancePosition.getZ() + 0.5);
+                });
+            }
+
+            if (source.isFromFalling()) {
+                cir.setReturnValue(false);
+            }
+        }
+    }
+}
