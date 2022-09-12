d5af4d92fc2ac07dc3c5804b3356a962af93c5a3
Herman Mesky <dr.gmes@gmail.com>
2022-09-12 11:53:08 +0300

    Updated Engineer trades and texture

--- src/main/java/net/drgmes/dwm/setup/ModVillagerProfessions.java
+++ src/main/java/net/drgmes/dwm/setup/ModVillagerProfessions.java
@@ -20,19 +20,23 @@ public class ModVillagerProfessions {
         ENGINEER.setTradeOffers(ImmutableMap.of(
             1,
             new TradeOffers.Factory[]{
-                new TradeOffers.SellItemFactory(ModItems.REPAIR_KIT.getItem(), 12, 1, 1, 10),
+                new TradeOffers.SellItemFactory(ModItems.TARDIS_KEY_1.getItem(), 64, 1, 1, 10),
+                new TradeOffers.SellItemFactory(ModItems.REPAIR_KIT.getItem(), 12, 1, 1, 2),
             },
             2,
             new TradeOffers.Factory[]{
-                new TradeOffers.BuyForOneEmeraldFactory(Items.AMETHYST_SHARD, 1, 16, 2),
+                new TradeOffers.BuyForOneEmeraldFactory(Items.AMETHYST_SHARD, 1, 8, 4),
+                new TradeOffers.BuyForOneEmeraldFactory(ModItems.TITANIUM_INGOT.getItem(), 1, 16, 2),
+            },
+            3,
+            new TradeOffers.Factory[]{
                 new TradeOffers.SellItemFactory(ModItems.SCREWDRIVER_11.getItem(), 48, 1, 1, 60),
                 new TradeOffers.SellItemFactory(ModItems.SCREWDRIVER_12.getItem(), 48, 1, 1, 60),
                 new TradeOffers.SellItemFactory(ModItems.SCREWDRIVER_13.getItem(), 48, 1, 1, 60),
             },
-            3,
+            4,
             new TradeOffers.Factory[]{
-                new TradeOffers.BuyForOneEmeraldFactory(Items.ECHO_SHARD, 1, 8, 4),
-                new TradeOffers.SellItemFactory(ModItems.TARDIS_KEY_1.getItem(), 64, 1, 1, 60),
+                new TradeOffers.BuyForOneEmeraldFactory(Items.ECHO_SHARD, 1, 4, 10),
             }
         ));
     }
