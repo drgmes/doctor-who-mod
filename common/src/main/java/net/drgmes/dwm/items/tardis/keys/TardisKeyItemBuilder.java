5d5010dc52f3993466dedd239ee43c8f47e429b0
Herman Mesky <drgmes@gmail.com>
2023-06-12 19:02:28 +0300

    Added way to reset relation from Tardis Key

--- common/src/main/java/net/drgmes/dwm/items/tardis/keys/TardisKeyItemBuilder.java
+++ common/src/main/java/net/drgmes/dwm/items/tardis/keys/TardisKeyItemBuilder.java
@@ -3,7 +3,13 @@ package net.drgmes.dwm.items.tardis.keys;
 import net.drgmes.dwm.DWM;
 import net.drgmes.dwm.datagen.ItemModelDataBuilder;
 import net.drgmes.dwm.utils.builders.ItemBuilder;
+import net.drgmes.dwm.utils.helpers.RecipeHelper;
+import net.minecraft.data.server.recipe.RecipeJsonProvider;
+import net.minecraft.data.server.recipe.ShapelessRecipeJsonBuilder;
 import net.minecraft.item.Item;
+import net.minecraft.recipe.book.RecipeCategory;
+
+import java.util.function.Consumer;
 
 public class TardisKeyItemBuilder extends ItemBuilder {
     public TardisKeyItemBuilder(String name) {
@@ -17,5 +23,13 @@ public class TardisKeyItemBuilder extends ItemBuilder {
     @Override
     public ItemModelDataBuilder getItemModelDataBuilder() {
         return new ItemModelDataBuilder(this.getItem(), this.getId(), DWM.getIdentifier("item/tardis/keys/" + this.getName()), ItemModelDataBuilder.ItemType.PARENTED);
+    }
+
+    @Override
+    public void registerRecipe(Consumer<RecipeJsonProvider> exporter) {
+        ShapelessRecipeJsonBuilder.create(RecipeCategory.MISC, this.getItem(), 1)
+            .input(this.getItem())
+            .criterion("has_item", RecipeHelper.conditionsFromItem(this.getItem()))
+            .offerTo(exporter);
     }
 }
