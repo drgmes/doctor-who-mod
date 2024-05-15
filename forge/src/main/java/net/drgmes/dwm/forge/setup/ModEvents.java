e99a35508992c96a3e4928e7cca59cc97eed3a14
Herman Mesky <drgmes@gmail.com>
2024-05-16 00:54:41 +0300

    Fixed clientside issue (forge)

--- forge/src/main/java/net/drgmes/dwm/forge/setup/ModEvents.java
+++ forge/src/main/java/net/drgmes/dwm/forge/setup/ModEvents.java
@@ -2,6 +2,8 @@ package net.drgmes.dwm.forge.setup;
 
 import net.drgmes.dwm.DWM;
 import net.drgmes.dwm.common.sonicdevice.modes.scan.SonicDeviceScanModeOverlay;
+import net.minecraftforge.api.distmarker.Dist;
+import net.minecraftforge.api.distmarker.OnlyIn;
 import net.minecraftforge.client.event.RenderGuiOverlayEvent;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
 import net.minecraftforge.fml.common.Mod;
@@ -9,6 +11,7 @@ import net.minecraftforge.fml.common.Mod;
 @Mod.EventBusSubscriber(modid = DWM.MODID)
 public class ModEvents {
     @SubscribeEvent
+    @OnlyIn(Dist.CLIENT)
     public static void onRenderGuiOverlay(RenderGuiOverlayEvent.Post event) {
         SonicDeviceScanModeOverlay.INSTANCE.render(event.getGuiGraphics());
     }
