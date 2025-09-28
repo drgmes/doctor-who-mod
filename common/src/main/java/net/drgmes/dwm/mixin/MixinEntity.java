8286defe4e53ba611b4b33ea1d33ec7d7ad33c4d
drgmes <drgmes@gmail.com>
2025-09-28 17:26:55 +0000

    Improved cancelling void damage mixins

--- common/src/main/java/net/drgmes/dwm/mixin/MixinEntity.java
+++ common/src/main/java/net/drgmes/dwm/mixin/MixinEntity.java
@@ -3,42 +3,27 @@ package net.drgmes.dwm.mixin;
 import net.drgmes.dwm.common.tardis.TardisStateManager;
 import net.drgmes.dwm.utils.helpers.EntityHelper;
 import net.drgmes.dwm.utils.helpers.TardisHelper;
-import net.minecraft.entity.LivingEntity;
-import net.minecraft.entity.damage.DamageSource;
-import net.minecraft.entity.damage.DamageTypes;
+import net.minecraft.entity.Entity;
 import net.minecraft.server.world.ServerWorld;
 import net.minecraft.util.math.Vec3d;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
-import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 
-@Mixin(LivingEntity.class)
-public class MixinLivingEntity {
+@Mixin(Entity.class)
+public class MixinEntity {
     @Inject(method = "tickInVoid", at = @At("HEAD"), cancellable = true)
     private void tickInVoid(CallbackInfo ci) {
-        LivingEntity $this = (LivingEntity) (Object) this;
+        Entity $this = (Entity) (Object) this;
         if (!TardisHelper.isTardisDimension($this.getWorld())) return;
 
         ci.cancel();
         if ($this.getWorld().isClient) return;
 
         TardisStateManager.get((ServerWorld) $this.getWorld()).ifPresent((tardis) -> {
-            $this.fallDistance = 0;
-
             Vec3d pos = Vec3d.ofBottomCenter(tardis.getEntrancePosition().offset(tardis.getEntranceFacing()));
             EntityHelper.teleport($this, tardis.getWorld(), pos, tardis.getEntranceFacing().asRotation());
         });
-    }
-
-    @Inject(method = "damage", at = @At("HEAD"), cancellable = true)
-    private void damage(DamageSource source, float amount, CallbackInfoReturnable<Boolean> cir) {
-        LivingEntity $this = (LivingEntity) (Object) this;
-        if (!TardisHelper.isTardisDimension($this.getWorld())) return;
-
-        if (source.isOf(DamageTypes.FALL) || source.isOf(DamageTypes.IN_WALL)) {
-            cir.setReturnValue(false);
-        }
     }
 }
